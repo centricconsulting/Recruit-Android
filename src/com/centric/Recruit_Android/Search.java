76d043c642267ea5c0466726d4b7ddd4e59323f4
Michael D. Brooks <mike.brooks@centricconsulting.com>
2013-07-17 01:33:33 -0400

    Inital Project Add

---
+++ src/com/centric/Recruit_Android/Search.java
@@ -0,0 +1,17 @@
+package com.centric.Recruit_Android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Mike Brooks
+ * Date: 7/17/13
+ * Time: 1:05 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Search extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
